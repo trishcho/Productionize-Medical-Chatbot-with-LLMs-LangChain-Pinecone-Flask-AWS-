ccfee9108edd49e94da82d3f100bfa2109e4afbe
trishcho <trishulchowdhury.23@gmail.com>
2025-08-25 09:24:37 -0400

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Trishul Chowdhury",
+    author_email="trishulchowdhury.23@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
